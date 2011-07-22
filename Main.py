8bfc99c680bd1c7022a273cac0a7998c891f89f6
Christopher Käck <christopher.kack@gmail.com>
2011-07-22 14:25:47 +0200

    Migrated to OO.

--- Main.py
+++ Main.py
@@ -11,33 +11,7 @@ def get_og(name, soup):
 	else:
 		return "No "+ name + " tag found"
 
-if __name__ != '__main__':
-	print "Initiating parse"
-	if len(sys.argv) > 2: 
-		print "Missing URL. Terminating"
-		exit()
-	else:
-		print "Contacting " + sys.argv[1]
-		url = sys.argv[1]
-		
-	try:
-		content = urllib2.urlopen(url).read()
-		print "Succecfully read the website"
-	except urllib2.URLError: 
-		print "The URL speficied was not valid. Terminating"
-		exit()
-	soup = BeautifulSoup.BeautifulSoup(content)
-	print "----------------------------------"
-	print "Title " + get_og("og:title", soup)
-	print "Type " + get_og("og:type", soup)
-	print "Image " + get_og("og:image", soup)
-	print "URL " + get_og("og:url", soup)
-	print "Description " + get_og("og:description", soup)
-	print "Site name " + get_og("og:site_name", soup)
-	print "Email " + get_og("og:email", soup)
-	print "Phone Number " + get_og("og:phone_number", soup)
-	print "----------------------------------"
-else:
+if __name__ == '__main__':
 	print "Initiating parse : CORRECT"
 	if len(sys.argv) > 2 : 
 		print "Missing URL. Terminating"
